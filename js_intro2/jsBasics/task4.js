cc116349d242746039412864b6fa25d7e20ab063
LIza <lizasusoreva292@gmail.com>
2025-09-27 22:00:10 +0300

    Added task 1-4

---
+++ js_intro2/jsBasics/task4.js
@@ -0,0 +1,22 @@
+// task 4.1
+
+let radius = 5;
+let circleSquare = Math.PI * radius * radius;
+
+console.log(Math.floor(circleSquare * 100) / 100);
+
+
+// task 4.2
+let length = 20;
+let width = 13;
+let rectangleSquare = length * width;
+
+console.log(rectangleSquare);
+
+
+//task 4.3
+let radiusCylinder = 11.32412432424324234;
+let height = 57.123123;
+let V = Math.PI * radiusCylinder * radiusCylinder * height;
+
+console.log((Math.floor(V * 100) / 100));
