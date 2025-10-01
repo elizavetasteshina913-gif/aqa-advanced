e58f9f13ac18dc140a4468d9bb5ba7069d67c6d8
LIza <lizasusoreva292@gmail.com>
2025-10-01 22:45:16 +0300

    add task 5.1

---
+++ .idea/homework/task3.js
@@ -0,0 +1,13 @@
+const number = 5
+
+
+for (let i = 1; i <=10; i++) {
+    console.log (`${number} * ${i} = ${number * i}`);
+}
+
+
+let i = 1
+while (i <=10 ) {
+console.log (`${number} * ${i} = ${number * i}`)
+    i++;
+}
