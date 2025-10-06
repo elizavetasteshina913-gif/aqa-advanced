28395e89bedd6b40e6fb80f78fcdb7b5e14b0a29
LIza <lizasusoreva292@gmail.com>
2025-10-06 18:19:22 +0300

    add task 6.1

---
+++ .idea/homework/task2.js
@@ -0,0 +1,12 @@
+const countAge = function (age) {
+    if (age >= 18) {
+        console.log("it is adult")
+    }
+    else if (age < 18) {
+        console.log("it is kid")
+    }
+}
+
+
+countAge(25)
+countAge(15)
